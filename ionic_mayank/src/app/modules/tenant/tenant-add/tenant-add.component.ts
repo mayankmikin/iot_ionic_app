fe74194c6c9f89910c828b3faa4c453a9ef33edc
MayankVerma <mayank.verma@upstartcommerce.com>
2019-02-25 19:20:04 +0530

    added add and remove copmponent and back button in add new page

---
+++ ionic_mayank/src/app/modules/tenant/tenant-add/tenant-add.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tenant-add',
+  templateUrl: './tenant-add.component.html',
+  styleUrls: ['./tenant-add.component.scss'],
+})
+export class TenantAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
