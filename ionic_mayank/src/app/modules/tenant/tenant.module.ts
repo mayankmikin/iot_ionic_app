2f4e6abbcf40f4451258d88ecfcca2c8db92367c
MayankVerma <mayank.verma@upstartcommerce.com>
2019-02-26 10:53:13 +0530

    adding search bar

--- ionic_mayank/src/app/modules/tenant/tenant.module.ts
+++ ionic_mayank/src/app/modules/tenant/tenant.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -30,6 +30,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [TenantPage,TenantViewComponent,TenantAddComponent,TenantRemoveComponent]
