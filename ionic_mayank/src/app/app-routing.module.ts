5178974030bae0d6c68c15d3a95ad666859cb8af
Mayank <mayank.mikin@gmail.com>
2019-06-26 15:28:59 +0530

    adding todays lesson

--- ionic_mayank/src/app/app-routing.module.ts
+++ ionic_mayank/src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
   },
-  { path: 'tenant', loadChildren: './modules/tenant/tenant.module#TenantPageModule' }
+  { path: 'tenant', loadChildren: './modules/tenant/tenant.module#TenantPageModule' },
+  { path: 'device', loadChildren: './modules/device/device.module#DevicePageModule' }
 ];
 
 @NgModule({
---
+++ ionic_mayank/src/app/model/device.ts
@@ -0,0 +1,31 @@
+export class Device {
+     deviceId: string;
+
+    name: string;
+
+    exporterId: string;
+
+     manufacturerId: string;
+
+
+     serialId: string;
+
+
+     createdAt: string;
+
+
+     updatedAt: string;
+
+
+     account: string;
+
+     userDefinedName: string;
+
+     localIp: string;
+
+     externalIp: string;
+
+     ports: string[];
+
+
+}
